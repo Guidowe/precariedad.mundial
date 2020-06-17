52c07938f607dfb2fec6bba54b32704f46afedbd
Facundo Lastra <facundol@hotmail.com>
2020-06-17 14:40:54 -0300

    Actualizo

--- scripts/LFS CEPED - Cuadros.R
+++ scripts/LFS CEPED - Cuadros.R
@@ -33,14 +33,16 @@ while (i < length(countries) + 1) {
     summarise('pais'                                 = countries[i],
               'total.ocupados'                       = sum(WEIGHT, na.rm=TRUE),
               'tasa.asalarizacion'                   = sum(WEIGHT[STAPRO==3], na.rm=TRUE)/sum(WEIGHT, na.rm=TRUE),
-              'tasa.1.ocupados'                      = sum(WEIGHT[PRECACOUNT==1 | PRECACOUNT==2 | PRECACOUNT==3], na.rm=TRUE)/sum(WEIGHT, na.rm=TRUE),
               'total.asal'                           = sum(WEIGHT[STAPRO==3], na.rm=TRUE),
               'promedio.decil'                       = weighted.mean(INCDECIL[STAPRO==3], WEIGHT[STAPRO==3], na.rm=TRUE),            
+              'tasa.partime.asal'                    = sum(WEIGHT[PRECAPT==1 & STAPRO==3], na.rm=TRUE)/sum(WEIGHT[STAPRO==3], na.rm=TRUE),         
+              'tasa.temp.asal'                       = sum(WEIGHT[PRECATEMP==1 & STAPRO==3], na.rm=TRUE)/sum(WEIGHT[STAPRO==3], na.rm=TRUE), 
+              'tasa.subhora.asal'                    = sum(WEIGHT[PRECAHORA==1 & STAPRO==3], na.rm=TRUE)/sum(WEIGHT[STAPRO==3], na.rm=TRUE), 
               'tasa.1.asalariados'                   = sum(WEIGHT[PRECACOUNT==1 | PRECACOUNT==2 | PRECACOUNT==3], na.rm=TRUE)/sum(WEIGHT, na.rm=TRUE),
               'tasa.2.asalariados'                   = sum(WEIGHT[PRECACOUNT==2 | PRECACOUNT==3], na.rm=TRUE)/sum(WEIGHT, na.rm=TRUE),              
               'tasa.3.asalariados'                   = sum(WEIGHT[PRECACOUNT==3], na.rm=TRUE)/sum(WEIGHT, na.rm=TRUE),
               'total.tcp'                            = sum(WEIGHT[STAPRO==0], na.rm=TRUE),              
-              'tasa.parttime.tcp'                    = sum(WEIGHT[PRECAPT==1 & STAPRO==0], na.rm=TRUE)/sum(WEIGHT, na.rm=TRUE))  %>%
+              'tasa.parttime.tcp'                    = sum(WEIGHT[PRECAPT==1 & STAPRO==0], na.rm=TRUE)/sum(WEIGHT[STAPRO==0], na.rm=TRUE))  %>%
     ungroup() %>%
     group_by(YEAR) %>%
     mutate('particip.ocup'          = total.ocupados/sum(total.ocupados), 
@@ -51,7 +53,7 @@ while (i < length(countries) + 1) {
   colnames(Resultados)[1] <- "ANO4"
   colnames(Resultados)[2] <- "grupos.tamanio"
   colnames(Resultados)[3] <- "grupos.calif"
-  Resultados <-  Resultados[,c(4, 1, 2, 3, 5, 15, 6:8, 16, 9:13, 17, 14)]
+  Resultados <-  Resultados[,c(4, 1, 2, 3, 5, 17, 6:8, 18, 9:14, 19, 15, 16)]
   
   
   Resultados_bind <-   bind_rows(Resultados, Resultados_bind)
@@ -85,11 +87,10 @@ while (i < length(countries) + 1) {
   i <- i+1
 }
 
-  Desocup.Calif <- inner_join(Desocup.Calif_bind, Ocup.Calif_bind, by=c("Pais", "CALIF")) %>%
+Desocup.Calif <- inner_join(Desocup.Calif_bind, Ocup.Calif_bind, by=c("Pais", "CALIF")) %>%
     mutate(brecha=Por.Desocup/Por.Ocup)
   
   
-
 save(Resultados_bind, Desocup.Calif,
      file = paste0(CarpetaRdos,"EUROPA.RDATA"))
 
--- scripts/LFS SES Imputacion salario.R
+++ scripts/LFS SES Imputacion salario.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 
 
 ### PROMEDIO DE CADA DECIL
@@ -19,31 +19,17 @@ PromedioHorarioDeciles <- SES                                                   
   spread(PAIS, Promedio)                                                        %>% 
   select(-decilhorario)
 
-
-Base <- readRDS("F:/LFS/BasesUnificadas/SeleccionPaises2014.Rda")
-
-BaseImputada <- data.frame()
-
 #Saco a SE sin datos para deciles
 PromedioDeciles <- PromedioDeciles %>% select(-SE2014)
 PromedioHorarioDeciles <- PromedioHorarioDeciles %>% select(-SE2014)
 #No incluyo a Grecia que no está en SES
 countries <- c("BG", "DE", "DK", "ES", "FR", "IT", "PT",  "RO", "UK")
 
-#Genero variable para identificar el pais sin ano
-Base <-  Base                        %>%
-  mutate(pais=substr(COUNTRY, 1, 2))   
-
-
-i <- 1
 while (i < length(countries) + 1) {
+  
+  Base <- readRDS(paste0(CarpetaBasesUnificadas, countries[i], "2008-2018.Rda"))
 
-#Imputo deciles pais por pais
-
-Basep <- Base %>%
-  filter(pais==countries[i])
-
-Basep <- Basep  %>%
+Base <- Base  %>%
   mutate(salarioNAC= case_when(
                             INCDECIL==1  ~ PromedioDeciles[[1,i]],
                             INCDECIL==2  ~ PromedioDeciles[[2,i]],
@@ -56,7 +42,7 @@ Basep <- Basep  %>%
                             INCDECIL==9  ~ PromedioDeciles[[9,i]],
                             INCDECIL==10 ~ PromedioDeciles[[10,i]]))
 
-Basep <- Basep  %>%
+Base <- Base  %>%
   mutate(salariohorarioNAC= case_when(
     INCDECIL==1  ~ PromedioHorarioDeciles[[1,i]],
     INCDECIL==2  ~ PromedioHorarioDeciles[[2,i]],
@@ -69,40 +55,36 @@ Basep <- Basep  %>%
     INCDECIL==9  ~ PromedioHorarioDeciles[[9,i]],
     INCDECIL==10 ~ PromedioHorarioDeciles[[10,i]]))
 
-BaseImputada <- bind_rows(BaseImputada, Basep)
+Base <- Base %>%
+  mutate(salarioNAC        = case_when(YEAR==2014 ~ salarioNAC), 
+         salariohorarioNAC = case_when(YEAR==2014 ~ salariohorarioNAC))
+
+Base <- Base %>%
+  mutate('salario'= case_when( COUNTRY=="ES" ~ salarioNAC/0.738510185, 
+                               COUNTRY=="FR" ~ salarioNAC/0.832556816    ,
+                               COUNTRY=="IT" ~ salarioNAC/0.853587053    ,
+                               COUNTRY=="PT" ~ salarioNAC/0.687558871    ,
+                               COUNTRY=="UK" ~ salarioNAC/0.656065473    ,
+                               COUNTRY=="RO" ~ salarioNAC/1.217568971    ,
+                               COUNTRY=="SE" ~ salarioNAC/8.447913694    ,
+                               COUNTRY=="DE" ~ salarioNAC/0.834523744    ,
+                               COUNTRY=="DK" ~ salarioNAC/8.552694151    ,
+                               COUNTRY=="BG"  ~ 0), 
+         'salariohorario'= case_when( COUNTRY=="ES" ~ salariohorarioNAC/0.738510185, 
+                                      COUNTRY=="FR" ~ salariohorarioNAC/0.832556816    ,
+                                      COUNTRY=="IT" ~ salariohorarioNAC/0.853587053    ,
+                                      COUNTRY=="PT" ~ salariohorarioNAC/0.687558871    ,
+                                      COUNTRY=="UK" ~ salariohorarioNAC/0.656065473    ,
+                                      COUNTRY=="RO" ~ salariohorarioNAC/1.217568971    ,
+                                      COUNTRY=="SE" ~ salariohorarioNAC/8.447913694    ,
+                                      COUNTRY=="DE" ~ salariohorarioNAC/0.834523744    ,
+                                      COUNTRY=="DK" ~ salariohorarioNAC/8.552694151    ,
+                                      COUNTRY=="BG"  ~ 0))
+    
+saveRDS(Base, paste0(CarpetaBasesUnificadas, countries[i], "2008-2018.Rda"))    
 
 i <- i + 1
 
 }
 
 
-### PASO A PPP
-
-GR <- Base %>% filter(pais=="GR")
-SE <- Base %>% filter(pais=="SE")
-
-BaseImputada <- bind_rows(BaseImputada, GR, SE)
-
-BaseImputada <- BaseImputada %>%
-  mutate('salario'= case_when( COUNTRY=="ES" ~ salarioNAC/0.738510185, 
-                             COUNTRY=="FR" ~ salarioNAC/0.832556816    ,
-                             COUNTRY=="IT" ~ salarioNAC/0.853587053    ,
-                             COUNTRY=="PT" ~ salarioNAC/0.687558871    ,
-                             COUNTRY=="UK" ~ salarioNAC/0.656065473    ,
-                             COUNTRY=="RO" ~ salarioNAC/1.217568971    ,
-                             COUNTRY=="SE" ~ salarioNAC/8.447913694    ,
-                             COUNTRY=="DE" ~ salarioNAC/0.834523744    ,
-                             COUNTRY=="DK" ~ salarioNAC/8.552694151    ,
-                             COUNTRY=="BG"  ~ 0), 
-         'salariohorario'= case_when( COUNTRY=="ES" ~ salariohorarioNAC/0.738510185, 
-                                    COUNTRY=="FR" ~ salariohorarioNAC/0.832556816    ,
-                                    COUNTRY=="IT" ~ salariohorarioNAC/0.853587053    ,
-                                    COUNTRY=="PT" ~ salariohorarioNAC/0.687558871    ,
-                                    COUNTRY=="UK" ~ salariohorarioNAC/0.656065473    ,
-                                    COUNTRY=="RO" ~ salariohorarioNAC/1.217568971    ,
-                                    COUNTRY=="SE" ~ salariohorarioNAC/8.447913694    ,
-                                    COUNTRY=="DE" ~ salariohorarioNAC/0.834523744    ,
-                                    COUNTRY=="DK" ~ salariohorarioNAC/8.552694151    ,
-                                    COUNTRY=="BG"  ~ 0)) 
-
-saveRDS(BaseImputada, "F:/LFS/BasesUnificadas/SeleccionPaises2014.Rda")
